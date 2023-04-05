b81ff3995740ce3aa0f37209c50d0fdc4d43031a
procrastimax <mmeheykeroth@protonmail.com>
2023-04-05 02:07:05 +0200

    Improves ema smoothing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use clap::Parser;
-use reqwest::blocking::get;
 use srgb;
-use std::fmt::format;
-use std::io::Write;
 use std::net::UdpSocket;
 use std::{thread::sleep, time::Duration};
 use x11cap::*;
@@ -16,16 +13,13 @@ struct Args {
     #[arg(
         short,
         long,
-        default_value_t = 300,
-        help = "wait time before updating RGB values in ms"
+        default_value_t = 5,
+        help = "delay before updating RGB values in ms"
     )]
-    wait_delay: u64,
+    delay: u64,
 
     #[arg(short, long, default_value_t = false)]
     use_linear_rgb: bool,
-
-    #[arg(short, long, default_value_t = String::from("http"))]
-    protocol: String,
 
     #[arg(short, long, default_value_t = 0)]
     monitor: u8,
@@ -38,13 +32,8 @@ struct Args {
     )]
     threshold: u64,
 
-    #[arg(
-        short,
-        long,
-        default_value_t = 0.0,
-        help = "a factor specifying the change rate for the EMA smoothing with a window of 2, value of 0.0 disables smoothing"
-    )]
-    smooth_factor: f32,
+    #[arg(short, long, default_value_t = 15)]
+    window_length: i32,
 }
 
 #[toml_cfg::toml_config]
@@ -63,24 +52,24 @@ fn main() {
         endpoint = SETTINGS.stripe_server_endpoint.to_string();
     }
 
-    println!(
-        "Starting RGB LED Blacklight to endpoint: {}://{}...",
-        args.protocol, endpoint
-    );
+    println!("Starting RGB LED Blacklight to endpoint: {}...", endpoint);
     let mut capturer = Capturer::new(CaptureSource::Monitor(args.monitor.into())).unwrap();
+    let geo = capturer.get_geometry();
+    let size = geo.width as u64 * geo.height as u64;
 
     let (mut last_r, mut last_g, mut last_b) = (0, 0, 0);
 
     let udp_stream = UdpSocket::bind("0.0.0.0:8777").unwrap();
     udp_stream.connect(format!("{}:80", endpoint)).unwrap();
+    println!("conected...");
 
-    println!("conected via udp!");
+    let mut ema_r: u8 = 0;
+    let mut ema_g: u8 = 0;
+    let mut ema_b: u8 = 0;
+    let alpha: f32 = 2.0 / (args.window_length as f32 + 1.0);
 
     loop {
         let ps = capturer.capture_frame().unwrap();
-
-        let geo = capturer.get_geometry();
-        let size = geo.width as u64 * geo.height as u64;
 
         let (mut tot_r, mut tot_g, mut tot_b) = (0, 0, 0);
 
@@ -102,37 +91,18 @@ fn main() {
             tot_b = (b * 255.0) as u8;
         }
 
-        if args.smooth_factor > 0.0 {
-            // exponentially weighted moving average calculation
-            let w_1 = 1.0 - args.smooth_factor;
-            let w_2 = (1.0 - args.smooth_factor).powi(2);
+        ema_r = (alpha * tot_r as f32 + (1.0 - alpha) * ema_r as f32) as u8;
+        ema_g = (alpha * tot_g as f32 + (1.0 - alpha) * ema_g as f32) as u8;
+        ema_b = (alpha * tot_b as f32 + (1.0 - alpha) * ema_b as f32) as u8;
 
-            tot_r = (((last_r as f32 * w_1) + (tot_r as f32 * w_2)) / (w_1 + w_2)).round() as u8;
-            tot_g = (((last_g as f32 * w_1) + (tot_g as f32 * w_2)) / (w_1 + w_2)).round() as u8;
-            tot_b = (((last_b as f32 * w_1) + (tot_b as f32 * w_2)) / (w_1 + w_2)).round() as u8;
-        }
-
-        let curr_sum: u64 = tot_r as u64 + tot_g as u64 + tot_b as u64;
+        let curr_sum: u64 = ema_r as u64 + ema_g as u64 + ema_b as u64;
         let last_sum: u64 = last_r as u64 + last_g as u64 + last_b as u64;
 
         if curr_sum.abs_diff(last_sum) > args.threshold {
-            // UDP
-            udp_stream
-                .send(format!("r={},g={},b={}\n", tot_r, tot_g, tot_b).as_bytes())
-                .unwrap();
-
-            // HTTP
-            //    let request_str = format!(
-            //        "{}://{}/setRGBA?r={}&g={}&b={}",
-            //        args.protocol, endpoint, tot_r, tot_g, tot_b
-            //    );
-            //    let resp = get(request_str);
-            //    if resp.is_err() {
-            //        eprintln!("{:?}", resp.err());
-            //    }
+            let package = format!("r={},g={},b={}\n", ema_r, ema_g, ema_b);
+            udp_stream.send(package.as_bytes()).unwrap();
         }
-
-        (last_r, last_g, last_b) = (tot_r, tot_g, tot_b);
-        sleep(Duration::from_millis(args.wait_delay));
+        (last_r, last_g, last_b) = (ema_r, ema_g, ema_b);
+        sleep(Duration::from_millis(args.delay));
     }
 }
